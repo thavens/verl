e6431ca54ec0564dcb4933749908d814541285a9
Michael Lavery <michael.lavery.2017@gmail.com>
2025-08-29 23:07:00 -0700

    add new ppo recipes

---
+++ experiments/gemma3_1b/g_1b_struq_ap.sh
@@ -0,0 +1,58 @@
+output_dir=${1:-/storage_fast/models/michael_lavery}
+
+set -xeuo pipefail
+
+project_name='verl_grpo_pir'
+experiment_name='g3_1b_struq_ap'
+
+uv run python -m verl.trainer.main_ppo \
+    algorithm.adv_estimator=gae \
+    data.train_files=./pir_struq_ap.parquet \
+    data.val_files=./pir_struq_ap.parquet \
+    data.train_batch_size=64 \
+    data.max_prompt_length=1024 \
+    data.max_response_length=3072 \
+    data.filter_overlong_prompts=True \
+    data.truncation='error' \
+    actor_rollout_ref.model.path=/scratch/public_models/huggingface/Qwen/Qwen2.5-0.5B-Instruct \
+    actor_rollout_ref.model.use_liger=False \
+    actor_rollout_ref.model.use_fused_kernels=True \
+    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr_warmup_steps=10 \
+    actor_rollout_ref.model.use_remove_padding=False \
+    actor_rollout_ref.actor.ppo_mini_batch_size=32 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=8 \
+    actor_rollout_ref.actor.use_kl_loss=False \
+    actor_rollout_ref.actor.kl_loss_coef=0.0 \
+    actor_rollout_ref.actor.entropy_coeff=0 \
+    actor_rollout_ref.model.enable_gradient_checkpointing=True \
+    actor_rollout_ref.actor.fsdp_config.param_offload=False \
+    actor_rollout_ref.actor.fsdp_config.optimizer_offload=False \
+    actor_rollout_ref.rollout.log_prob_micro_batch_size_per_gpu=64 \
+    actor_rollout_ref.rollout.tensor_model_parallel_size=1 \
+    actor_rollout_ref.rollout.name=vllm \
+    actor_rollout_ref.rollout.gpu_memory_utilization=0.5 \
+    actor_rollout_ref.rollout.n=16 \
+    actor_rollout_ref.ref.fsdp_config.param_offload=True \
+    critic.optim.lr=1e-5 \
+    critic.model.use_remove_padding=False \
+    critic.model.path=/scratch/public_models/huggingface/Qwen/Qwen2.5-0.5B-Instruct \
+    critic.model.enable_gradient_checkpointing=True \
+    critic.ppo_micro_batch_size_per_gpu=8 \
+    critic.model.fsdp_config.param_offload=False \
+    critic.model.fsdp_config.optimizer_offload=False \
+    algorithm.use_kl_in_reward=False \
+    trainer.val_before_train=False \
+    trainer.critic_warmup=10 \
+    trainer.logger='["console","wandb"]' \
+    trainer.project_name="${project_name}" \
+    trainer.experiment_name="${experiment_name}" \
+    trainer.n_gpus_per_node=2 \
+    trainer.nnodes=1 \
+    trainer.save_freq=60 \
+    trainer.test_freq=-1 \
+    trainer.total_epochs=2 \
+    trainer.total_training_steps=180 \
+    custom_reward_function.path=pir_reward.py \
+    custom_reward_function.name=no_think \
+    trainer.default_local_dir="${output_dir}/${project_name}/${experiment_name}"
